e93c8512b8b9ded9ec7a327150efb9f8c51a3f3d
sonht <sonht1109@gmail.com>
2021-01-05 14:44:26 +0700

    create folders

---
+++ src/common/Layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Menu from '../components/Menu'
+
+export default function Layout() {
+    return (
+        <div>
+            <Menu />
+        </div>
+    )
+}
---
+++ src/components/Menu.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Navbar, NavbarBrand} from 'reactstrap'
+
+export default function Menu() {
+    return (
+        <Navbar href="#" color="dark" dark expand="md">
+            <NavbarBrand>CND</NavbarBrand>
+        </Navbar>
+    )
+}
